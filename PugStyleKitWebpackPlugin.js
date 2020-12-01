7f1e23ce956c19627c40a9b434eda1afa4420156
小野沢　敦 <01029278@waku-2.com>
2020-12-01 11:26:44 +0900

    オプションを渡してみる

--- PugStyleKitWebpackPlugin.js
+++ PugStyleKitWebpackPlugin.js
@@ -1,10 +1,30 @@
+const schemaUnits = require('schema-utils');
+
+
+const schema = {
+  type: 'object',
+  properties: {
+    test: {
+      type: 'string'
+    }
+  }
+};
+
+const PLUGIN_NAME = 'PugStyleKitWebpackPlugin';
+
 class PugStyleKitWebpackPlugin {
+
+  constructor(options = {}){
+    schemaUnits.validate(schema, options, { name: PLUGIN_NAME });
+    this.options = options;
+  }
+
   apply(compiler) {
-    const PLUGIN_NAME = 'PugStyleKitWebpackPlugin';
 
     compiler.hooks.beforeRun.tapAsync(PLUGIN_NAME, (compiler, callback) => {
       console.log(PLUGIN_NAME);
       console.log('this is before run hook!');
+      console.log(this.options);
       callback();
     });
 
