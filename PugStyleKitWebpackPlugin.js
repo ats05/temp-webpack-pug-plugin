3f7ec1f486ce7230d9a34c0da57fdfad384ca810
小野沢　敦 <01029278@waku-2.com>
2020-12-01 20:23:50 +0900

    WIP stylekitタグのparse作業

--- PugStyleKitWebpackPlugin.js
+++ PugStyleKitWebpackPlugin.js
@@ -3,6 +3,8 @@ const {Compilation, sources: {RawSource}} = require('webpack');
 const path = require('path');
 const pug = require('pug');
 const fs = require('fs');
+const pugLexer = require('pug-lexer');
+const pugParser = require('pug-parser');
 
 
 const schema = {
@@ -34,6 +36,9 @@ class PugStyleKitWebpackPlugin {
 
       compilation.hooks.processAssets.tapPromise(PLUGIN_NAME, async () => {
 
+        const regexTarget = /stylekit\./g;
+        const regexReplace = '//-';
+
         const context = compilation.compiler.context;
         let options = this.options;
         const outputPath = compilation.compiler.outputPath;
@@ -42,6 +47,17 @@ class PugStyleKitWebpackPlugin {
 
         options.forEach(config => {
           const buffer = fs.readFileSync(config.from, 'utf8');
+          const filename = path.basename(config.from);
+
+          const tokens = pugLexer(buffer, {filename});
+          const ast = this._parseFile(config.from);
+
+          const targetNames = ['stylekit'];
+          const styleKitElements = this._findElementsByName(ast, targetNames);
+          console.log(styleKitElements);
+          // ここでタグをコメントに置き換える
+
+
           const options = {
             filename: config.from,
             basedir: path.dirname(config.from)
@@ -53,6 +69,39 @@ class PugStyleKitWebpackPlugin {
         });
       });
     });
+  }
+
+  _parseFile(filename) {
+    const buffer = fs.readFileSync(filename, 'utf8');
+    const tokens = pugLexer(buffer, {filename});
+    const ast = pugParser(tokens, {filename, buffer});
+    return ast;
+  }
+
+  _findElementsByName(currentNode, targetNames, list = new Set()){
+    let nodes = [];
+
+    if(targetNames.includes(currentNode.name)) {
+      list.add(currentNode);
+    }
+
+    if('file' in currentNode) {
+      const childFilePath = path.join(path.dirname(currentNode.filename), currentNode.file.path);
+      const ast = this._parseFile(childFilePath);
+      nodes = ast.nodes;
+    }
+    else if('nodes' in currentNode && currentNode.nodes.length > 0) {
+      nodes = currentNode.nodes;
+    }
+    else if('block' in currentNode && currentNode.block != null) {
+      nodes = currentNode.block.nodes;
+    }
+
+    nodes.forEach(node => {
+      this._findElementsByName(node, targetNames, list);
+    });
+
+    return list;
   }
 }
 
